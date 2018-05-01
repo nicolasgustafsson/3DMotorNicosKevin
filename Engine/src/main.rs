ac041529300e6b3e3bd1789aa0c50b8ab619466f
Nico <itsarabbit@gmail.com>
2018-05-01 16:57:21 +0200

    second coming of triangle

--- Engine/src/main.rs
+++ Engine/src/main.rs
@@ -8,48 +8,37 @@ extern crate vulkano_shader_derive;
 
 mod vulkano_win_frankenstein;
 
-/*use vulkano::instance::Instance;
-use vulkano::device::Device;
-
-
-//use vulkano::swapchain;
-use vulkano::swapchain::PresentMode;
-use vulkano::swapchain::SurfaceTransform;
-use vulkano::swapchain::Swapchain;
-//use vulkano::swapchain::AcquireError;
-//use vulkano::swapchain::SwapchainCreationError;
-
-use vulkano::pipeline::GraphicsPipeline;
-
-use vulkano::framebuffer::Subpass;
-
-use vulkano::buffer::BufferUsage;
-use vulkano::buffer::CpuAccessibleBuffer;
-
-use std::sync::Arc;*/
-
-use vulkano::buffer::BufferUsage;
-use vulkano::buffer::CpuAccessibleBuffer;
-use vulkano::command_buffer::AutoCommandBufferBuilder;
-use vulkano::command_buffer::DynamicState;
-use vulkano::device::Device;
-use vulkano::framebuffer::Framebuffer;
-use vulkano::framebuffer::Subpass;
 use vulkano::instance::Instance;
-use vulkano::pipeline::GraphicsPipeline;
-use vulkano::pipeline::viewport::Viewport;
+use vulkano::device::Device;
+
 use vulkano::swapchain;
 use vulkano::swapchain::PresentMode;
 use vulkano::swapchain::SurfaceTransform;
 use vulkano::swapchain::Swapchain;
-use vulkano::swapchain::AcquireError;
 use vulkano::swapchain::SwapchainCreationError;
+use vulkano::swapchain::AcquireError;
+
+
+use vulkano::command_buffer::AutoCommandBufferBuilder;
+use vulkano::command_buffer::DynamicState;
+
+use vulkano::pipeline::GraphicsPipeline;
+use vulkano::pipeline::viewport::Viewport;
+
+use vulkano::framebuffer::Framebuffer;
+use vulkano::framebuffer::Subpass;
+
+use vulkano::buffer::BufferUsage;
+use vulkano::buffer::CpuAccessibleBuffer;
+
 use vulkano::sync::now;
 use vulkano::sync::GpuFuture;
 
 use std::sync::Arc;
 use std::mem;
 
+use std::time::Instant;
+use std::time::Duration;
 
 use vulkano_win_frankenstein::vulkano_win_frankenstein::VkSurfaceBuild;
 
@@ -114,22 +103,23 @@ fn main() {
             &graphics_queue, 
             SurfaceTransform::Identity, 
             alpha_mode, 
-            PresentMode::Fifo, 
+            PresentMode::Mailbox, 
             true, 
             None).expect("Could not create swapchain!")
     };
 
-    #[derive(Debug, Clone)]
-    struct Vertex { position: [f32; 2] }
-    impl_vertex!(Vertex, position);
+        #[derive(Debug, Clone)]
+        struct Vertex { position: [f32; 2] }
+        impl_vertex!(Vertex, position);
 
-    let vertex_buffer = 
+    let mut vertex_buffer = 
     {
+
         CpuAccessibleBuffer::from_iter(device.clone(), BufferUsage::all(), [
             Vertex { position: [-0.5, -0.25] },
             Vertex { position: [0.0, 0.5] },
             Vertex { position: [0.25, -0.1] }
-        ].iter()).expect("Could not create vertex buffer!");
+        ].iter().cloned()).expect("Could not create vertex buffer!")
     };
 
     mod vs {
@@ -142,7 +132,7 @@ void main() {
     gl_Position = vec4(position, 0.0, 1.0);
 }
 "]
-        struct Dummy;
+        struct _Dummy;
     }
 
     mod fs {
@@ -155,7 +145,7 @@ void main() {
     f_color = vec4(1.0, 0.0, 0.0, 1.0);
 }
 "]
-        struct Dummy;
+        struct _Dummy;
     }
 
     let vs = vs::Shader::load(device.clone()).expect("Could not create vertex shader module!");
@@ -188,54 +178,57 @@ void main() {
         .build(device.clone())
         .unwrap());
 
-    let mut run = true;
+    let mut framebuffers : Option<Vec<Arc<vulkano::framebuffer::Framebuffer<_,_>>>> = None;
 
-
-
-    /*WELP START */
-        let mut framebuffers: Option<Vec<Arc<vulkano::framebuffer::Framebuffer<_,_>>>> = None;
-
-    // Initialization is finally finished!
-
-    // In some situations, the swapchain will become invalid by itself. This includes for example
-    // when the window is resized (as the images of the swapchain will no longer match the
-    // window's) or, on Android, when the application went to the background and goes back to the
-    // foreground.
-    //
-    // In this situation, acquiring a swapchain image or presenting it will return an error.
-    // Rendering to an image of that swapchain will not produce any error, but may or may not work.
-    // To continue rendering, we need to recreate the swapchain by creating a new swapchain.
-    // Here, we remember that we need to do this for the next loop iteration.
     let mut recreate_swapchain = false;
 
-    // In the loop below we are going to submit commands to the GPU. Submitting a command produces
-    // an object that implements the `GpuFuture` trait, which holds the resources for as long as
-    // they are in use by the GPU.
-    //
-    // Destroying the `GpuFuture` blocks until the GPU is finished executing it. In order to avoid
-    // that, we store the submission of the previous frame here.
     let mut previous_frame_end = Box::new(now(device.clone())) as Box<GpuFuture>;
 
-    loop {
-        // It is important to call this function from time to time, otherwise resources will keep
-        // accumulating and you will eventually reach an out of memory error.
-        // Calling this function polls various fences in order to determine what the GPU has
-        // already processed, and frees the resources that are no longer needed.
+    let mut run = true;
+
+    let start = Instant::now();
+    let mut previous_frame_time = Instant::now();
+
+    while run
+    {
+        let now = Instant::now();
+
+
+        let mut time_elapsed = 0f32;
+        // we sleep for 2 seconds
+        time_elapsed = (now.duration_since(start).subsec_nanos() as f32) * 0.000000001f32 + now.duration_since(start).as_secs() as f32;
+
+        let nanoseconds : u32 = now.duration_since(previous_frame_time).subsec_nanos();
+
+        println!("FPS = {}", 1000000000f64 / (nanoseconds as f64));
+        
+        previous_frame_time = now;
+
+    
+        vertex_buffer = 
+        {
+                    CpuAccessibleBuffer::from_iter(device.clone(), BufferUsage::all(), [
+            Vertex { position: [-0.5, -0.25] },
+            Vertex { position: [time_elapsed.sin(), 0.5] },
+            Vertex { position: [0.25, -0.1] }
+        ].iter().cloned()).expect("Could not create vertex buffer!")
+        };
+
         previous_frame_end.cleanup_finished();
 
-        // If the swapchain needs to be recreated, recreate it
-        if recreate_swapchain {
-            // Get the new dimensions for the viewport/framebuffers.
-            dimensions = {
-                let (new_width, new_height) = window.window().get_inner_size().unwrap();
-                [new_width, new_height]
+        if recreate_swapchain
+        {
+            dimensions = 
+            {
+                let (width, height) = window.window().get_inner_size().expect("Could not get window inner size!");
+                [width, height]
             };
 
-            let (new_swapchain, new_images) = match swapchain.recreate_with_dimension(dimensions) {
+            let (new_swapchain, new_images) = match swapchain.recreate_with_dimension(dimensions) 
+            {
                 Ok(r) => r,
-                // This error tends to happen when the user is manually resizing the window.
-                // Simply restarting the loop is the easiest way to fix this issue.
-                Err(SwapchainCreationError::UnsupportedDimensions) => {
+                Err(SwapchainCreationError::UnsupportedDimensions) => 
+                {
                     continue;
                 },
                 Err(err) => panic!("{:?}", err)
@@ -249,125 +242,52 @@ void main() {
             recreate_swapchain = false;
         }
 
-        // Because framebuffers contains an Arc on the old swapchain, we need to
-        // recreate framebuffers as well.
-        if framebuffers.is_none() {
-            let new_framebuffers = Some(images.iter().map(|image| {
-                Arc::new(Framebuffer::start(render_pass.clone())
-                         .add(image.clone()).unwrap()
-                         .build().unwrap())
+        if framebuffers.is_none()
+        {
+            let new_framebuffers = Some(images.iter().map(|image| 
+            {
+                Arc::new(Framebuffer::start(render_pass.clone()).add(image.clone()).unwrap().build().unwrap())
             }).collect::<Vec<_>>());
+
             mem::replace(&mut framebuffers, new_framebuffers);
         }
 
-        // Before we can draw on the output, we have to *acquire* an image from the swapchain. If
-        // no image is available (which happens if you submit draw commands too quickly), then the
-        // function will block.
-        // This operation returns the index of the image that we are allowed to draw upon.
-        //
-        // This function can block if no image is available. The parameter is an optional timeout
-        // after which the function call will return an error.
-        let (image_num, acquire_future) = match swapchain::acquire_next_image(swapchain.clone(),
-                                                                              None) {
+        let (image_index, acquire_future) = match swapchain::acquire_next_image(swapchain.clone(), None)
+        {
             Ok(r) => r,
-            Err(AcquireError::OutOfDate) => {
+            Err(AcquireError::OutOfDate) => 
+            {
                 recreate_swapchain = true;
                 continue;
             },
             Err(err) => panic!("{:?}", err)
         };
 
-        // In order to draw, we have to build a *command buffer*. The command buffer object holds
-        // the list of commands that are going to be executed.
-        //
-        // Building a command buffer is an expensive operation (usually a few hundred
-        // microseconds), but it is known to be a hot path in the driver and is expected to be
-        // optimized.
-        //
-        // Note that we have to pass a queue family when we create the command buffer. The command
-        // buffer will only be executable on that given queue family.
         let command_buffer = AutoCommandBufferBuilder::primary_one_time_submit(device.clone(), graphics_queue.family()).unwrap()
-            // Before we can draw, we have to *enter a render pass*. There are two methods to do
-            // this: `draw_inline` and `draw_secondary`. The latter is a bit more advanced and is
-            // not covered here.
-            //
-            // The third parameter builds the list of values to clear the attachments with. The API
-            // is similar to the list of attachments when building the framebuffers, except that
-            // only the attachments that use `load: Clear` appear in the list.
-            .begin_render_pass(framebuffers.as_ref().unwrap()[image_num].clone(), false,
-                               vec![[0.0, 0.0, 1.0, 1.0].into()])
-            .unwrap()
-
-            // We are now inside the first subpass of the render pass. We add a draw command.
-            //
-            // The last two parameters contain the list of resources to pass to the shaders.
-            // Since we used an `EmptyPipeline` object, the objects have to be `()`.
+            .begin_render_pass(framebuffers.as_ref().unwrap()[image_index].clone(), false, 
+            vec![[0.0, 0.0, 1.0, 1.0].into()]).unwrap()
             .draw(pipeline.clone(),
-                  DynamicState {
-                      line_width: None,
-                      // TODO: Find a way to do this without having to dynamically allocate a Vec every frame.
-                      viewports: Some(vec![Viewport {
-                          origin: [0.0, 0.0],
-                          dimensions: [dimensions[0] as f32, dimensions[1] as f32],
-                          depth_range: 0.0 .. 1.0,
-                      }]),
-                      scissors: None,
-                  },
-                  vertex_buffer.clone(), (), ())
+            DynamicState{
+                line_width: None,
+                viewports: Some(vec![Viewport {
+                    origin: [0.0, 0.0],
+                    dimensions: [dimensions[0] as f32, dimensions[1] as f32],
+                    depth_range: 0.0 .. 1.0,
+                }]),
+                scissors: None,
+            },
+            vertex_buffer.clone(), (), ())
             .unwrap()
-
-            // We leave the render pass by calling `draw_end`. Note that if we had multiple
-            // subpasses we could have called `next_inline` (or `next_secondary`) to jump to the
-            // next subpass.
             .end_render_pass()
-            .unwrap()
-
-            // Finish building the command buffer by calling `build`.
-            .build().unwrap();
+            .unwrap().build().unwrap();
 
         let future = previous_frame_end.join(acquire_future)
-            .then_execute(graphics_queue.clone(), command_buffer).unwrap()
+        .then_execute(graphics_queue.clone(), command_buffer).unwrap()
+        .then_swapchain_present(graphics_queue.clone(), swapchain.clone(), image_index)
+        .then_signal_fence_and_flush().unwrap();
 
-            // The color output is now expected to contain our triangle. But in order to show it on
-            // the screen, we have to *present* the image by calling `present`.
-            //
-            // This function does not actually present the image immediately. Instead it submits a
-            // present command at the end of the queue. This means that it will only be presented once
-            // the GPU has finished executing the command buffer that draws the triangle.
-            .then_swapchain_present(graphics_queue.clone(), swapchain.clone(), image_num)
-            .then_signal_fence_and_flush().unwrap();
         previous_frame_end = Box::new(future) as Box<_>;
 
-        // Note that in more complex programs it is likely that one of `acquire_next_image`,
-        // `command_buffer::submit`, or `present` will block for some time. This happens when the
-        // GPU's queue is full and the driver has to wait until the GPU finished some work.
-        //
-        // Unfortunately the Vulkan API doesn't provide any way to not wait or to detect when a
-        // wait would happen. Blocking may be the desired behavior, but if you don't want to
-        // block you should spawn a separate thread dedicated to submissions.
-
-        // Handling the window events in order to close the program when the user wants to close
-        // it.
-        let mut done = false;
-        //events_loop.poll_events(|ev| {
-        //    match ev {
-        //        winit::Event::WindowEvent { event: winit::WindowEvent::Closed, .. } => done = true,
-        //        winit::Event::WindowEvent { event: winit::WindowEvent::Resized(_, _), .. } => recreate_swapchain = true,
-        //        _ => ()
-        //    }
-        //});
-        //if done { return; 
-        }
-
-        //WELP END
-
-
-
-
-
-
-    while run
-    {
         event_loop.poll_events(|event| 
         {
             match event 
